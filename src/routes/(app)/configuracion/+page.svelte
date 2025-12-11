6f1cfd226377b3e498f109bf898c901a872fa6a2
madmti <supermatrix9@gmail.com>
2025-12-11 02:39:14 -0300

    feat: added config page with Semestres manager component

--- src/routes/(app)/configuracion/+page.svelte
+++ src/routes/(app)/configuracion/+page.svelte
@@ -1,7 +1,27 @@
 <script>
 	import { fly } from 'svelte/transition';
+	import Semestres from './components/Semestres.svelte';
+	import Sincronizacion from './components/Sincronizacion.svelte';
+	import Aparicencia from './components/Apariencia.svelte';
+	import About from './components/About.svelte';
 </script>
 
-<div in:fly={{ y: 10, duration: 300, delay: 100 }}>
-	<h1>configuracion</h1>
+<div in:fly={{ y: 10, duration: 300, delay: 100 }} class="p-4 w-full">
+	<div class="columns-1 lg:columns-2 gap-6 space-y-6">
+		<div class="break-inside-avoid">
+			<Semestres />
+		</div>
+
+		<div class="break-inside-avoid">
+			<Aparicencia />
+		</div>
+
+		<div class="break-inside-avoid">
+			<Sincronizacion />
+		</div>
+
+		<div class="break-inside-avoid">
+			<About />
+		</div>
+	</div>
 </div>
