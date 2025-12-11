a3242dd26f3d95271247d53c31f8f9a9bd652e88
madmti <supermatrix9@gmail.com>
2025-12-11 02:37:21 -0300

    feat: added Page Tittle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import './custom.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.webp" />
+	<title>Ramo Libre</title>
 </svelte:head>
 
-<div class="h-full w-full flex flex-col">
+<div class="h-full w-full flex flex-col max-sm:flex-col-reverse">
 	{@render children()}
 </div>
