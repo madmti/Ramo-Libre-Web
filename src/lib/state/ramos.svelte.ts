4f0f9faf94fbca15bb27c7a8400b77a1e6505263
madmti <supermatrix9@gmail.com>
2025-12-11 17:58:00 -0300

    feat: added ramos state manager and some methods to Serializable

---
+++ src/lib/state/ramos.svelte.ts
@@ -0,0 +1,61 @@
+import type { Serializable } from '$lib/types/state';
+import { SvelteMap } from 'svelte/reactivity';
+
+interface Ramo {
+	nombre: string;
+	color: string;
+	estado?: 'possible' | 'impossible' | 'guaranteed';
+}
+type Key = string;
+type RamosSerial = [Key, Ramo][];
+type Ramos = SvelteMap<Key, Ramo>;
+
+export class RamosManager implements Serializable<RamosSerial> {
+	private _ramos = $state<Ramos>(new SvelteMap<Key, Ramo>());
+
+	fromSerial(serial: RamosSerial) {
+		this._ramos = new SvelteMap<Key, Ramo>(serial);
+	}
+
+	toSerial(): RamosSerial {
+		return Array.from(this._ramos.entries());
+	}
+
+	clear(): void {
+	    this._ramos.clear();
+	}
+
+	empty(): boolean {
+        return this._ramos.size === 0;
+    }
+
+	add(ramo: Ramo) {
+		const id = crypto.randomUUID();
+		this._ramos.set(id, ramo);
+		return id;
+	}
+
+	remove(id: string) {
+		this._ramos.delete(id);
+	}
+
+	get(id: string): Ramo | undefined {
+		return this._ramos.get(id);
+	}
+
+	has(id: string): boolean {
+		return this._ramos.has(id);
+	}
+
+	update(id: string, ramo: Ramo) {
+		this._ramos.set(id, ramo);
+	}
+
+	get list() {
+		return Array.from(this._ramos.entries());
+	}
+
+	get map() {
+		return this._ramos;
+	}
+}
